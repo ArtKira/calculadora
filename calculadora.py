f3337bcb5e75041877abe36be369ca3f953c65fc
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2022-09-28 09:38:16 -0500

    Se agrega las opciones para el resultado

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,3 @@
-from email import message
-
 
 def suma(num1, num2):
     return num1+num2
@@ -14,9 +12,26 @@ def div(num1, num2):
     return num1/num2
 
 if __name__ == '__main__':
-    message=f"Calculadora:  Elige la opcion 1=Suma,  2=Resta,  3=Multiplicacion, 4=Divison, 5=Para salir "
+    message=f"Calculadora:\n  Elige la opcion\n 1=Suma\n  2=Resta\n  3=Multiplicacion\n 4=Divison\n 5=Para salir\n"
     while True:
         option=int(input(message))
-        if option == 5:
+        #compara cada opcion y llama a la funcion correspondiente 
+        num1=int(input("Ingresa un numero "))
+        num2=int(input("Ingresa un segundo numero "))
+        if option == 1:
+            resultado_suma=suma(num1,num2)
+            print("el resultado de la suma es ", resultado_suma)
+        elif option== 2:
+            resultado_resta=resta(num1,num2)
+            print("el resultado de la resta es ", resultado_resta)
+        if option==3:
+            resultado_multi=multi(num1,num2)
+            print("el resultado de la multiplicacion es ", resultado_multi)
+        if option==4:
+            resultado_div=div(num1,num2)
+            print("el resultado de la division es ", resultado_div)
+        elif option == 5:
             print("BYE")
             break
+        else:
+            print ("Opcion incorrecta")
